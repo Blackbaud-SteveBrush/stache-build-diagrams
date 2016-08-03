5ea93d3c22d6a5942fd3690f92e354061fa43500
stevebrush <steve.brush@blackbaud.com>
2016-08-03 11:44:38 -0400

    Added hotfix flow

--- src/scripts/app.js
+++ src/scripts/app.js
@@ -166,8 +166,13 @@
         name: 'feature',
         column: 2
     });
-    feature.commit('Updated tutorial');
-    feature.merge(master);
+    feature.commit({
+        message: 'Updated tutorial'
+    });
+    feature.merge(master, {
+        author: 'UserEd',
+        message: '[Pull Request] Merge branch `feature` into `master`'
+    });
     master.merge(deployTest, {
         author: 'Travis-CI',
         message: "[Trigger deploy to test]"
@@ -180,7 +185,10 @@
         column: 2
     });
     changelog.commit('Added endpoints and breaking change');
-    changelog.merge(master);
+    changelog.merge(master, {
+        author: 'UserEd',
+        message: '[Pull Request] Merge branch `changelog` into `master`'
+    });
     master.merge(deployTest, {
         author: 'Travis-CI',
         message: "[Trigger deploy to test]"
@@ -209,5 +217,102 @@
         message: "[Trigger deploy to prod]"
     });
     release.delete();
+
+}(window.GitGraph));
+
+
+(function (GitGraph) {
+    'use strict';
+
+    var deployProd,
+        deployTest,
+        graph,
+        hotfix,
+        master,
+        template;
+
+    template = new GitGraph.Template({
+        branch: {
+            color: "#000000",
+            lineWidth: 3,
+            spacingX: 60,
+            mergeStyle: "bezier",
+            showLabel: true,
+            labelFont: "normal 10pt Arial"
+        },
+        commit: {
+            spacingY: -30,
+            dot: {
+                size: 8,
+                strokeColor: "#000000",
+                strokeWidth: 4
+            },
+            tag: {
+                font: "normal 10pt Arial",
+                color: "yellow"
+            },
+            message: {
+                color: "black",
+                font: "normal 12pt Arial",
+                displayBranch: false,
+                displayHash: false,
+            }
+        }
+    });
+
+    graph = new GitGraph({
+        author: 'UserEd',
+        elementId: 'git-graph-hotfix',
+        template: template
+    });
+
+    master = graph.branch({
+        name: 'master',
+        column: 1
+    });
+
+    master.commit({
+        author: 'UserEd',
+        message: 'Initial commit',
+        messageBranchDisplay: false,
+        messageHashDisplay: false
+    });
+
+    deployTest = master.branch({
+        name: 'deploy-test',
+        column: 2
+    });
+    deployTest.commit({
+        message: "Initial commit",
+        messageDisplay: false
+    });
+    deployProd = master.branch({
+        name: 'deploy-prod',
+        column: 4
+    });
+    deployProd.commit({
+        message: "Initial commit",
+        messageDisplay: false
+    });
+
+
+
+    hotfix = deployProd.branch({
+        name: 'hotfix',
+        column: 3
+    });
+    hotfix.commit({
+        message: 'Fixed issue with production'
+    });
+    hotfix.merge(deployProd, {
+        author: 'Travis-CI',
+        message: "[Trigger deploy to prod]"
+    });
+    hotfix.merge(master);
+
+    master.merge(deployTest, {
+        author: 'Travis-CI',
+        message: "[Trigger deploy to test]"
+    });
 
 }(window.GitGraph));
